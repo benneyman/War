2ba83376bccccefbf3c6807a3943bcf85da3d259
Bennet Sunder <ben.sunder@gmail.com>
2019-07-16 17:21:02 +0530

    edit run files to install dependencies and run tests

--- run-linux.sh
+++ run-linux.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 if command -v python -3 &>/dev/null; then
-    py -3 main.py
+    echo Installing Dependencies
+    pip3 install -r requirements/dev.txt
+    echo Running Tests
+    python3 -m pytest tests/
+    echo Running App
+    python3 main.py
 else
     echo Python 3 is not installed
 fi
--- run-windows.bat
+++ run-windows.bat
@@ -2,6 +2,11 @@
 :: Check for Python Installation
 py -3 --version >nul
 if errorlevel 1 goto errorNoPython
+    echo Installing Dependencies
+    pip3 install -r requirements/dev.txt
+    echo Running Tests
+    py -3 -m pytest tests/
+    echo Running App
     py -3 main.py
     pause
 goto:eof
